ad4a03f5cbe6213e131773a4a19da9f12edcc916
archimedon <ronniedz@icloud.com>
2017-05-17 17:35:15 -0700

    Change LOGGER

--- VidLib_Youtube_pipes/src/main/java/cab/bean/srvcs/pipes/processor/CachePullProcessor.java
+++ VidLib_Youtube_pipes/src/main/java/cab/bean/srvcs/pipes/processor/CachePullProcessor.java
@@ -1,8 +1,6 @@
 package cab.bean.srvcs.pipes.processor;
 
 import java.lang.reflect.InvocationTargetException;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
@@ -10,10 +8,10 @@ import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
 import org.apache.commons.beanutils.BeanUtils;
 import org.apache.commons.lang3.StringUtils;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.bson.types.ObjectId;
 import org.mongojack.JacksonDBCollection;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import cab.bean.srvcs.pipes.PersistenceHelper;
 import cab.bean.srvcs.pipes.QueueException;
@@ -23,11 +21,10 @@ import cab.bean.srvcs.tube4kids.core.MongoVideo;
 
 import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
-import com.mongodb.DBCollection;
 import com.mongodb.DBObject;
 
 public class CachePullProcessor implements Processor {
-    private static final Log LOGGER = LogFactory.getLog(CachePullProcessor.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(CachePullProcessor.class);
     private final DB db;
     
     public CachePullProcessor(DB db) {
@@ -37,20 +34,21 @@ public class CachePullProcessor implements Processor {
     @Override
     public void process(Exchange exchange) throws QueueException {
 
-	LOGGER.debug("IN  - GET Headers(): " + exchange.getIn().getHeaders().keySet());
-	LOGGER.debug("OUT - GET Headers(): " + exchange.getOut().getHeaders().keySet());
-	
 	try {
 	
-	    final VideoSearchRequest outQuery = (VideoSearchRequest) exchange.getIn().getHeader(PersistenceHelper.HDR_NAME_SERVICE_DEST_DATA);
+	    final VideoSearchRequest outQuery =
+		    (VideoSearchRequest) exchange.getIn().getHeader(PersistenceHelper.HDR_NAME_SERVICE_DEST_DATA);
 	    
-	    final Map<String, String> params = exchange.getIn().getHeader(PersistenceHelper.HDR_QUERYPARAMS_NAME, Map.class);
+	    final Map<String, String> params =
+		    exchange.getIn().getHeader(PersistenceHelper.HDR_QUERYPARAMS_NAME, Map.class);
 	    
 	    final YouTubeResponse youTubeResponse =  new YouTubeResponse();		
 	    
 	    BeanUtils.copyProperties(youTubeResponse, outQuery);
 	    
-	    final DBObject query = StringUtils.isBlank(outQuery.getLastId()) ? new BasicDBObject() : new BasicDBObject("_id", new BasicDBObject("$gte", new ObjectId(outQuery.getLastId()))); 
+	    final DBObject query = StringUtils.isBlank(outQuery.getLastId())
+		    ? new BasicDBObject()
+	    	     : new BasicDBObject("_id", new BasicDBObject("$gte", new ObjectId(outQuery.getLastId()))); 
 	    
 	    final JacksonDBCollection<MongoVideo, String> jVidColl = JacksonDBCollection.wrap(
 		    db.getCollection(exchange.getIn().getHeader(PersistenceHelper.HDR_NAME_COLLECTION_NAME, String.class)),
@@ -60,6 +58,10 @@ public class CachePullProcessor implements Processor {
 		    jVidColl.find(query).limit(Integer.parseInt(Optional.ofNullable(params.get("maxResults")).orElse(PersistenceHelper.DEFAULT_FETCH_SIZE)))
 		    .toArray());
 	
+	    LOGGER.debug("From mongoCache ...:\n\tCollectionName: {}\n\tNum Items: {}",
+		    youTubeResponse.getCollectionName(),
+		    youTubeResponse.getItems().size());
+	    
 	    exchange.getOut().setBody(youTubeResponse);
 	}
 	catch ( IllegalAccessException | InvocationTargetException e ) {
